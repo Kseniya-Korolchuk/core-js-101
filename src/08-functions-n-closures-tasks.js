aec56b02adf21f967520c743f5fc998367243abb
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-07-25 20:25:52 +0300

    delete: getComposition task solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,11 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(f, g) {
-  // throw new Error('Not implemented');
-  return function (x) {
-    return f(g(x));
-  };
+function getComposition(/* f, g */) {
+  throw new Error('Not implemented');
 }
 
 
